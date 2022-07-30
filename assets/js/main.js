796d13b9946f681671820ecc5d9ce723a34f4f45
André Igor <andreigormenezes@gmail.com>
2022-07-30 16:55:20 -0300

    latest code fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,26 +6,12 @@ const calcBtn = document.querySelector('button');
 const result = document.querySelector('.result');
 
 function associarIMC(imc) {
-    if (imc < 18.5) {
-       return 'Abaixo do peso';
-    } 
+    if (imc < 18.5) return 'Abaixo do peso';    
+    if (imc <= 24.9) return 'Peso normal';     
+    if (imc <= 29.9) return 'Sobrepeso';    
+    if (imc <= 34.9) return 'Obesidade grau 1';    
+    if (imc <= 39.9) return 'Obesidade grau 2';
     
-    if (imc <= 24.9) {
-        return 'Peso normal';
-    } 
-    
-    if (imc <= 29.9) {
-        return 'Sobrepeso';
-    }
-    
-    if (imc <= 34.9) {
-        return 'Obesidade grau 1';
-    }
-    
-    if (imc <= 39.9) {
-        return 'Obesidade grau 2';
-    } 
-
     return 'Obesidade grau 3';
 }
 
